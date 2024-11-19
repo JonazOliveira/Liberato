737bedea43bfd57738c67e18a285d07db140d0d8
JonazOliveira <jonasdeoliveira91@gmail.com>
2024-11-18 22:57:51 -0300

    Project DB 01

---
+++ Linguagem I/ProjectDB01/ProjectDB01/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ProjectDB01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxProduto = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.database1DataSet = new ProjectDB01.Database1DataSet();
+            this.produtoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.produtoTableAdapter = new ProjectDB01.Database1DataSetTableAdapters.ProdutoTableAdapter();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.produtoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxProduto
+            // 
+            this.comboBoxProduto.FormattingEnabled = true;
+            this.comboBoxProduto.Location = new System.Drawing.Point(21, 12);
+            this.comboBoxProduto.Name = "comboBoxProduto";
+            this.comboBoxProduto.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxProduto.TabIndex = 0;
+            this.comboBoxProduto.SelectedIndexChanged += new System.EventHandler(this.comboBoxProduto_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "EXE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 118);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(519, 150);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // produtoBindingSource
+            // 
+            this.produtoBindingSource.DataMember = "Produto";
+            this.produtoBindingSource.DataSource = this.database1DataSet;
+            // 
+            // produtoTableAdapter
+            // 
+            this.produtoTableAdapter.ClearBeforeFill = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "EXE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 321);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxProduto);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.produtoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxProduto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource produtoBindingSource;
+        private Database1DataSetTableAdapters.ProdutoTableAdapter produtoTableAdapter;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Linguagem I/ProjectDB01/ProjectDB01/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectDB01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'database1DataSet.Produto'. Você pode movê-la ou removê-la conforme necessário.
+            this.produtoTableAdapter.Fill(this.database1DataSet.Produto);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // combobox
+                //faz a conexão sql
+                SqlConnection cn = new SqlConnection();
+
+                //seta a string de conexão
+                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\24100169\Desktop\Nova pasta\ProjectDB01\ProjectDB01\Database1.mdf;Integrated Security=True";
+
+                //abre a conexão
+                cn.Open();
+
+                //Cria o comando sql
+                SqlCommand cmd = new SqlCommand();
+
+                //string com sql
+                string sqlQuery = "select Id, descricao from produto order by descricao";
+
+                //passa a query para o comando e abre a conexão
+                cmd = new SqlCommand(sqlQuery, cn);
+
+                //cria o Sql Data Adapter
+                SqlDataAdapter dAdapter = new SqlDataAdapter();
+
+                //cria Data Table
+                DataTable dt = new DataTable();
+
+                //Incializa SQL Data Adapter Command Property
+                dAdapter.SelectCommand = cmd;
+
+                //Preenche Data Table
+                dAdapter.Fill(dt);
+
+                //Popula Combo Box com os dados do Data Table
+                comboBoxProduto.DataSource = dt;
+
+                //Seta combobox com o valor
+                comboBoxProduto.ValueMember = "Id";
+
+                //seta combobox com descrição
+                comboBoxProduto.DisplayMember = "descricao";
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void comboBoxProduto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // caminho da conexão na properiedade do banco de dados -- connection string
+                SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\24100169\Desktop\Nova pasta\ProjectDB01\ProjectDB01\Database1.mdf;Integrated Security=True");
+
+                // comando
+                SqlCommand comm = new SqlCommand();
+
+                // chama  a conexão
+                comm.Connection = conn;
+
+                // abre a conexao
+                conn.Open();
+                //MessageBox.Show("teste " + comboBox2.SelectedValue);
+
+                {
+                    using (SqlDataAdapter a = new SqlDataAdapter(
+                           "SELECT * FROM Cidade", conn)
+                           )
+                    {
+
+                        DataTable t = new DataTable();
+                        a.Fill(t);
+                        // Render data onto the screen
+                        dataGridView1.DataSource = t;
+                    }
+                }
+                // fecha conexao
+                conn.Close();
+            }
+            catch (Exception Ex)
+            {
+                // Show(Ex.Message);
+                MessageBox.Show(Ex.Message);
+            }
+        }
+    }
+}
