a9db1848bfec5d7f6b5b09cd5282594b8c3ff9d9
JonazOliveira <jonasdeoliveira91@gmail.com>
2024-11-19 22:45:21 -0300

    Project DB 02

---
+++ Linguagem I/ProjectDB02/ProjectDB02/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ProjectDB02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxPessoa = new System.Windows.Forms.ComboBox();
+            this.dataGridViewCartaoC = new System.Windows.Forms.DataGridView();
+            this.buttonPessoa = new System.Windows.Forms.Button();
+            this.buttonCartaoC = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCartaoC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxPessoa
+            // 
+            this.comboBoxPessoa.FormattingEnabled = true;
+            this.comboBoxPessoa.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxPessoa.Name = "comboBoxPessoa";
+            this.comboBoxPessoa.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPessoa.TabIndex = 0;
+            this.comboBoxPessoa.SelectedIndexChanged += new System.EventHandler(this.comboBoxPessoa_SelectedIndexChanged);
+            // 
+            // dataGridViewCartaoC
+            // 
+            this.dataGridViewCartaoC.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCartaoC.Location = new System.Drawing.Point(22, 98);
+            this.dataGridViewCartaoC.Name = "dataGridViewCartaoC";
+            this.dataGridViewCartaoC.Size = new System.Drawing.Size(444, 150);
+            this.dataGridViewCartaoC.TabIndex = 1;
+            this.dataGridViewCartaoC.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCartaoC_CellContentClick);
+            // 
+            // buttonPessoa
+            // 
+            this.buttonPessoa.Location = new System.Drawing.Point(170, 12);
+            this.buttonPessoa.Name = "buttonPessoa";
+            this.buttonPessoa.Size = new System.Drawing.Size(75, 23);
+            this.buttonPessoa.TabIndex = 2;
+            this.buttonPessoa.Text = "EXE";
+            this.buttonPessoa.UseVisualStyleBackColor = true;
+            this.buttonPessoa.Click += new System.EventHandler(this.buttonPessoa_Click);
+            // 
+            // buttonCartaoC
+            // 
+            this.buttonCartaoC.Location = new System.Drawing.Point(485, 165);
+            this.buttonCartaoC.Name = "buttonCartaoC";
+            this.buttonCartaoC.Size = new System.Drawing.Size(75, 23);
+            this.buttonCartaoC.TabIndex = 3;
+            this.buttonCartaoC.Text = "EXE";
+            this.buttonCartaoC.UseVisualStyleBackColor = true;
+            this.buttonCartaoC.Click += new System.EventHandler(this.buttonCartaoC_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCartaoC);
+            this.Controls.Add(this.buttonPessoa);
+            this.Controls.Add(this.dataGridViewCartaoC);
+            this.Controls.Add(this.comboBoxPessoa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCartaoC)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPessoa;
+        private System.Windows.Forms.DataGridView dataGridViewCartaoC;
+        private System.Windows.Forms.Button buttonPessoa;
+        private System.Windows.Forms.Button buttonCartaoC;
+    }
+}
+
---
+++ Linguagem I/ProjectDB02/ProjectDB02/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectDB02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPessoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //faz a conexão sql
+                SqlConnection cn = new SqlConnection();
+                //seta a string de conexão
+                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\24100169\Desktop\Nova pasta\ProjectDB02\ProjectDB02\Database1.mdf;Integrated Security=True";
+                //abre a conexão
+                cn.Open();
+                //Cria o comando sql
+                SqlCommand cmd = new SqlCommand();
+                //string com sql
+                string sqlQuery = "select Id, nome from pessoa order by nome";
+                //passa a query para o comando e abre a conexão
+                cmd = new SqlCommand(sqlQuery, cn);
+                //cria o Sql Data Adapter
+                SqlDataAdapter dAdapter = new SqlDataAdapter();
+                //cria Data Table
+                DataTable dt = new DataTable();
+                //Incializa SQL Data Adapter Command Property
+                dAdapter.SelectCommand = cmd;
+                //Preenche Data Table
+                dAdapter.Fill(dt);
+                //Popula Combo Box com os dados do Data Table
+                comboBoxPessoa.DataSource = dt;
+                //Seta combobox com o valor
+                comboBoxPessoa.ValueMember = "Id";
+                //seta combobox com descrição
+                comboBoxPessoa.DisplayMember = "nome";
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonCartaoC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // caminho da conexão na properiedade do banco de dados -- connection string
+                SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\24100169\Desktop\Nova pasta\ProjectDB02\ProjectDB02\Database1.mdf;Integrated Security=True");
+
+                // comando
+                SqlCommand comm = new SqlCommand();
+
+                // chama  a conexão
+                comm.Connection = conn;
+
+                // abre a conexao
+                conn.Open();
+                //MessageBox.Show("teste " + comboBox2.SelectedValue);
+
+                {
+                    using (SqlDataAdapter a = new SqlDataAdapter(
+                           "SELECT * FROM CartaoC WHERE CARTAOC.IDPESSOA = " + comboBoxPessoa.SelectedValue, conn)
+                           )
+                    {
+
+                        DataTable t = new DataTable();
+                        a.Fill(t);
+                        // Render data onto the screen
+                        dataGridViewCartaoC.DataSource = t;
+                    }
+                }
+                // fecha conexao
+                conn.Close();
+            }
+            catch (Exception Ex)
+            {
+                // Show(Ex.Message);
+                MessageBox.Show(Ex.Message);
+            }
+        }
+
+        private void comboBoxPessoa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewCartaoC_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
