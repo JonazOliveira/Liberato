f41e9da40cf81bf3cbb29483dc6a1fdd08188152
JonazOliveira <jonasdeoliveira91@gmail.com>
2024-11-25 23:28:45 -0300

    Prova Final

---
+++ Linguagem I/ProvaFinal/ProvaFinal/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ProvaFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelCidade01 = new System.Windows.Forms.Label();
+            this.labelPessoa01 = new System.Windows.Forms.Label();
+            this.comboBoxCidade01 = new System.Windows.Forms.ComboBox();
+            this.dataGridViewPessoa01 = new System.Windows.Forms.DataGridView();
+            this.buttonCidade01 = new System.Windows.Forms.Button();
+            this.buttonPessoa01 = new System.Windows.Forms.Button();
+            this.buttonPessoa02 = new System.Windows.Forms.Button();
+            this.buttonCidade02 = new System.Windows.Forms.Button();
+            this.dataGridViewPessoa02 = new System.Windows.Forms.DataGridView();
+            this.comboBoxCidade02 = new System.Windows.Forms.ComboBox();
+            this.labelPessoa02 = new System.Windows.Forms.Label();
+            this.labelCidade02 = new System.Windows.Forms.Label();
+            this.buttonEstadoCivil01 = new System.Windows.Forms.Button();
+            this.comboBoxEstadoCivil01 = new System.Windows.Forms.ComboBox();
+            this.labelEstadoCivil01 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPessoa01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPessoa02)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelCidade01
+            // 
+            this.labelCidade01.AutoSize = true;
+            this.labelCidade01.Location = new System.Drawing.Point(22, 39);
+            this.labelCidade01.Name = "labelCidade01";
+            this.labelCidade01.Size = new System.Drawing.Size(40, 13);
+            this.labelCidade01.TabIndex = 0;
+            this.labelCidade01.Text = "Cidade";
+            this.labelCidade01.Click += new System.EventHandler(this.labelCidade01_Click);
+            // 
+            // labelPessoa01
+            // 
+            this.labelPessoa01.AutoSize = true;
+            this.labelPessoa01.Location = new System.Drawing.Point(22, 105);
+            this.labelPessoa01.Name = "labelPessoa01";
+            this.labelPessoa01.Size = new System.Drawing.Size(42, 13);
+            this.labelPessoa01.TabIndex = 1;
+            this.labelPessoa01.Text = "Pessoa";
+            this.labelPessoa01.Click += new System.EventHandler(this.labelPessoa01_Click);
+            // 
+            // comboBoxCidade01
+            // 
+            this.comboBoxCidade01.FormattingEnabled = true;
+            this.comboBoxCidade01.Location = new System.Drawing.Point(90, 39);
+            this.comboBoxCidade01.Name = "comboBoxCidade01";
+            this.comboBoxCidade01.Size = new System.Drawing.Size(260, 21);
+            this.comboBoxCidade01.TabIndex = 2;
+            this.comboBoxCidade01.SelectedIndexChanged += new System.EventHandler(this.comboBoxCidade01_SelectedIndexChanged);
+            // 
+            // dataGridViewPessoa01
+            // 
+            this.dataGridViewPessoa01.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPessoa01.Location = new System.Drawing.Point(90, 105);
+            this.dataGridViewPessoa01.Name = "dataGridViewPessoa01";
+            this.dataGridViewPessoa01.Size = new System.Drawing.Size(260, 150);
+            this.dataGridViewPessoa01.TabIndex = 3;
+            this.dataGridViewPessoa01.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewPessoa01_CellContentClick);
+            // 
+            // buttonCidade01
+            // 
+            this.buttonCidade01.Location = new System.Drawing.Point(368, 39);
+            this.buttonCidade01.Name = "buttonCidade01";
+            this.buttonCidade01.Size = new System.Drawing.Size(75, 23);
+            this.buttonCidade01.TabIndex = 4;
+            this.buttonCidade01.Text = "EXE";
+            this.buttonCidade01.UseVisualStyleBackColor = true;
+            this.buttonCidade01.Click += new System.EventHandler(this.buttonCidade01_Click);
+            // 
+            // buttonPessoa01
+            // 
+            this.buttonPessoa01.Location = new System.Drawing.Point(368, 105);
+            this.buttonPessoa01.Name = "buttonPessoa01";
+            this.buttonPessoa01.Size = new System.Drawing.Size(75, 23);
+            this.buttonPessoa01.TabIndex = 5;
+            this.buttonPessoa01.Text = "EXE";
+            this.buttonPessoa01.UseVisualStyleBackColor = true;
+            this.buttonPessoa01.Click += new System.EventHandler(this.buttonPessoa01_Click);
+            // 
+            // buttonPessoa02
+            // 
+            this.buttonPessoa02.Location = new System.Drawing.Point(609, 422);
+            this.buttonPessoa02.Name = "buttonPessoa02";
+            this.buttonPessoa02.Size = new System.Drawing.Size(75, 23);
+            this.buttonPessoa02.TabIndex = 11;
+            this.buttonPessoa02.Text = "EXE";
+            this.buttonPessoa02.UseVisualStyleBackColor = true;
+            this.buttonPessoa02.Click += new System.EventHandler(this.buttonPessoa02_Click);
+            // 
+            // buttonCidade02
+            // 
+            this.buttonCidade02.Location = new System.Drawing.Point(609, 322);
+            this.buttonCidade02.Name = "buttonCidade02";
+            this.buttonCidade02.Size = new System.Drawing.Size(75, 23);
+            this.buttonCidade02.TabIndex = 10;
+            this.buttonCidade02.Text = "EXE";
+            this.buttonCidade02.UseVisualStyleBackColor = true;
+            this.buttonCidade02.Click += new System.EventHandler(this.buttonCidade02_Click);
+            // 
+            // dataGridViewPessoa02
+            // 
+            this.dataGridViewPessoa02.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPessoa02.Location = new System.Drawing.Point(90, 422);
+            this.dataGridViewPessoa02.Name = "dataGridViewPessoa02";
+            this.dataGridViewPessoa02.Size = new System.Drawing.Size(494, 150);
+            this.dataGridViewPessoa02.TabIndex = 9;
+            this.dataGridViewPessoa02.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewPessoa02_CellContentClick);
+            // 
+            // comboBoxCidade02
+            // 
+            this.comboBoxCidade02.FormattingEnabled = true;
+            this.comboBoxCidade02.Location = new System.Drawing.Point(90, 322);
+            this.comboBoxCidade02.Name = "comboBoxCidade02";
+            this.comboBoxCidade02.Size = new System.Drawing.Size(494, 21);
+            this.comboBoxCidade02.TabIndex = 8;
+            this.comboBoxCidade02.SelectedIndexChanged += new System.EventHandler(this.comboBoxCidade02_SelectedIndexChanged);
+            // 
+            // labelPessoa02
+            // 
+            this.labelPessoa02.AutoSize = true;
+            this.labelPessoa02.Location = new System.Drawing.Point(16, 422);
+            this.labelPessoa02.Name = "labelPessoa02";
+            this.labelPessoa02.Size = new System.Drawing.Size(42, 13);
+            this.labelPessoa02.TabIndex = 7;
+            this.labelPessoa02.Text = "Pessoa";
+            this.labelPessoa02.Click += new System.EventHandler(this.labelPessoa02_Click);
+            // 
+            // labelCidade02
+            // 
+            this.labelCidade02.AutoSize = true;
+            this.labelCidade02.Location = new System.Drawing.Point(16, 322);
+            this.labelCidade02.Name = "labelCidade02";
+            this.labelCidade02.Size = new System.Drawing.Size(40, 13);
+            this.labelCidade02.TabIndex = 6;
+            this.labelCidade02.Text = "Cidade";
+            this.labelCidade02.Click += new System.EventHandler(this.labelCidade02_Click);
+            // 
+            // buttonEstadoCivil01
+            // 
+            this.buttonEstadoCivil01.Location = new System.Drawing.Point(609, 366);
+            this.buttonEstadoCivil01.Name = "buttonEstadoCivil01";
+            this.buttonEstadoCivil01.Size = new System.Drawing.Size(75, 23);
+            this.buttonEstadoCivil01.TabIndex = 14;
+            this.buttonEstadoCivil01.Text = "EXE";
+            this.buttonEstadoCivil01.UseVisualStyleBackColor = true;
+            this.buttonEstadoCivil01.Click += new System.EventHandler(this.buttonEstadoCivil01_Click);
+            // 
+            // comboBoxEstadoCivil01
+            // 
+            this.comboBoxEstadoCivil01.FormattingEnabled = true;
+            this.comboBoxEstadoCivil01.Location = new System.Drawing.Point(90, 366);
+            this.comboBoxEstadoCivil01.Name = "comboBoxEstadoCivil01";
+            this.comboBoxEstadoCivil01.Size = new System.Drawing.Size(494, 21);
+            this.comboBoxEstadoCivil01.TabIndex = 13;
+            this.comboBoxEstadoCivil01.SelectedIndexChanged += new System.EventHandler(this.comboBoxEstadoCivil01_SelectedIndexChanged);
+            // 
+            // labelEstadoCivil01
+            // 
+            this.labelEstadoCivil01.AutoSize = true;
+            this.labelEstadoCivil01.Location = new System.Drawing.Point(16, 369);
+            this.labelEstadoCivil01.Name = "labelEstadoCivil01";
+            this.labelEstadoCivil01.Size = new System.Drawing.Size(62, 13);
+            this.labelEstadoCivil01.TabIndex = 12;
+            this.labelEstadoCivil01.Text = "Estado Civil";
+            this.labelEstadoCivil01.Click += new System.EventHandler(this.labelEstadoCivil01_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 617);
+            this.Controls.Add(this.buttonEstadoCivil01);
+            this.Controls.Add(this.comboBoxEstadoCivil01);
+            this.Controls.Add(this.labelEstadoCivil01);
+            this.Controls.Add(this.buttonPessoa02);
+            this.Controls.Add(this.buttonCidade02);
+            this.Controls.Add(this.dataGridViewPessoa02);
+            this.Controls.Add(this.comboBoxCidade02);
+            this.Controls.Add(this.labelPessoa02);
+            this.Controls.Add(this.labelCidade02);
+            this.Controls.Add(this.buttonPessoa01);
+            this.Controls.Add(this.buttonCidade01);
+            this.Controls.Add(this.dataGridViewPessoa01);
+            this.Controls.Add(this.comboBoxCidade01);
+            this.Controls.Add(this.labelPessoa01);
+            this.Controls.Add(this.labelCidade01);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPessoa01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPessoa02)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCidade01;
+        private System.Windows.Forms.Label labelPessoa01;
+        private System.Windows.Forms.ComboBox comboBoxCidade01;
+        private System.Windows.Forms.DataGridView dataGridViewPessoa01;
+        private System.Windows.Forms.Button buttonCidade01;
+        private System.Windows.Forms.Button buttonPessoa01;
+        private System.Windows.Forms.Button buttonPessoa02;
+        private System.Windows.Forms.Button buttonCidade02;
+        private System.Windows.Forms.DataGridView dataGridViewPessoa02;
+        private System.Windows.Forms.ComboBox comboBoxCidade02;
+        private System.Windows.Forms.Label labelPessoa02;
+        private System.Windows.Forms.Label labelCidade02;
+        private System.Windows.Forms.Button buttonEstadoCivil01;
+        private System.Windows.Forms.ComboBox comboBoxEstadoCivil01;
+        private System.Windows.Forms.Label labelEstadoCivil01;
+    }
+}
+
---
+++ Linguagem I/ProvaFinal/ProvaFinal/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProvaFinal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelCidade01_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelPessoa01_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxCidade01_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewPessoa01_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void buttonCidade01_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //faz a conexão sql
+                SqlConnection cn = new SqlConnection();
+                //seta a string de conexão
+                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\24100169\Desktop\Nova pasta\ProvaFinal\ProvaFinal\Database1.mdf;Integrated Security=True";
+                //abre a conexão
+                cn.Open();
+                //Cria o comando sql
+                SqlCommand cmd = new SqlCommand();
+                //string com sql
+                string sqlQuery = "SELECT IDCIDADE, NOMECIDADE FROM CIDADE ORDER BY NOMECIDADE;";
+                //passa a query para o comando e abre a conexão
+                cmd = new SqlCommand(sqlQuery, cn);
+                //cria o Sql Data Adapter
+                SqlDataAdapter dAdapter = new SqlDataAdapter();
+                //cria Data Table
+                DataTable dt = new DataTable();
+                //Incializa SQL Data Adapter Command Property
+                dAdapter.SelectCommand = cmd;
+                //Preenche Data Table
+                dAdapter.Fill(dt);
+                //Popula Combo Box com os dados do Data Table
+                comboBoxCidade01.DataSource = dt;
+                //Seta combobox com o valor
+                comboBoxCidade01.ValueMember = "IDCIDADE";
+                //seta combobox com descrição
+                comboBoxCidade01.DisplayMember = "NOMECIDADE";
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonPessoa01_Click(object sender, EventArgs e)
+        {
+            //popula grid view
+            try
+            {
+                // caminho da conexão na properiedade do banco de dados -- connection string
+                SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\24100169\Desktop\Nova pasta\ProvaFinal\ProvaFinal\Database1.mdf;Integrated Security=True");
+
+                // comando
+                SqlCommand comm = new SqlCommand();
+
+                // chama  a conexão
+                comm.Connection = conn;
+
+                // abre a conexao
+                conn.Open();
+                //MessageBox.Show("teste " + comboBox2.SelectedValue);
+
+                {
+                    using (SqlDataAdapter a = new SqlDataAdapter(
+                           "SELECT NOME, IDADE FROM PESSOA WHERE IDCIDADE = " + comboBoxCidade01.SelectedValue.ToString() + ";", conn)
+                           )
+                    {
+
+                        DataTable t = new DataTable();
+                        a.Fill(t);
+                        // Render data onto the screen
+                        dataGridViewPessoa01.DataSource = t;
+                    }
+                }
+                // fecha conexao
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                // Show(Ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void labelCidade02_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxCidade02_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCidade02_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //faz a conexão sql
+                SqlConnection cn = new SqlConnection();
+                //seta a string de conexão
+                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\24100169\Desktop\Nova pasta\ProvaFinal\ProvaFinal\Database1.mdf;Integrated Security=True";
+                //abre a conexão
+                cn.Open();
+                //Cria o comando sql
+                SqlCommand cmd = new SqlCommand();
+                //string com sql
+                string sqlQuery = "SELECT IDCIDADE, NOMECIDADE FROM CIDADE ORDER BY NOMECIDADE;";
+                //passa a query para o comando e abre a conexão
+                cmd = new SqlCommand(sqlQuery, cn);
+                //cria o Sql Data Adapter
+                SqlDataAdapter dAdapter = new SqlDataAdapter();
+                //cria Data Table
+                DataTable dt = new DataTable();
+                //Incializa SQL Data Adapter Command Property
+                dAdapter.SelectCommand = cmd;
+                //Preenche Data Table
+                dAdapter.Fill(dt);
+                //Popula Combo Box com os dados do Data Table
+                comboBoxCidade02.DataSource = dt;
+                //Seta combobox com o valor
+                comboBoxCidade02.ValueMember = "IDCIDADE";
+                //seta combobox com descrição
+                comboBoxCidade02.DisplayMember = "NOMECIDADE";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void labelEstadoCivil01_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxEstadoCivil01_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEstadoCivil01_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //faz a conexão sql
+                SqlConnection cn = new SqlConnection();
+                //seta a string de conexão
+                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\24100169\Desktop\Nova pasta\ProvaFinal\ProvaFinal\Database1.mdf;Integrated Security=True";
+                //abre a conexão
+                cn.Open();
+                //Cria o comando sql
+                SqlCommand cmd = new SqlCommand();
+                //string com sql
+                string sqlQuery = "SELECT IDESTADOC, DESCRICAO FROM ESTADOCIVIL ORDER BY DESCRICAO;";
+                //passa a query para o comando e abre a conexão
+                cmd = new SqlCommand(sqlQuery, cn);
+                //cria o Sql Data Adapter
+                SqlDataAdapter dAdapter = new SqlDataAdapter();
+                //cria Data Table
+                DataTable dt = new DataTable();
+                //Incializa SQL Data Adapter Command Property
+                dAdapter.SelectCommand = cmd;
+                //Preenche Data Table
+                dAdapter.Fill(dt);
+                //Popula Combo Box com os dados do Data Table
+                comboBoxEstadoCivil01.DataSource = dt;
+                //Seta combobox com o valor
+                comboBoxEstadoCivil01.ValueMember = "IDESTADOC";
+                //seta combobox com descrição
+                comboBoxEstadoCivil01.DisplayMember = "DESCRICAO";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void labelPessoa02_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewPessoa02_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void buttonPessoa02_Click(object sender, EventArgs e)
+        {
+            //popula grid view
+            try
+            {
+                // caminho da conexão na properiedade do banco de dados -- connection string
+                SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\24100169\Desktop\Nova pasta\ProvaFinal\ProvaFinal\Database1.mdf;Integrated Security=True");
+
+                // comando
+                SqlCommand comm = new SqlCommand();
+
+                // chama  a conexão
+                comm.Connection = conn;
+
+                // abre a conexao
+                conn.Open();
+                //MessageBox.Show("teste " + comboBox2.SelectedValue);
+
+                {
+                    using (SqlDataAdapter a = new SqlDataAdapter(
+                           "SELECT PESSOA.NOME, PESSOA.IDADE, CIDADE.NOMECIDADE, ESTADOCIVIL.DESCRICAO FROM PESSOA INNER JOIN CIDADE ON PESSOA.IDCIDADE = CIDADE.IDCIDADE INNER JOIN ESTADOCIVIL ON PESSOA.IDESTADOC = ESTADOCIVIL.IDESTADOC WHERE PESSOA.IDCIDADE = " + comboBoxCidade02.SelectedValue.ToString() + "AND PESSOA.IDESTADOC = " + comboBoxEstadoCivil01.SelectedValue.ToString() + ";", conn)
+                           )
+                    {
+
+                        DataTable t = new DataTable();
+                        a.Fill(t);
+                        // Render data onto the screen
+                        dataGridViewPessoa02.DataSource = t;
+                    }
+                }
+                // fecha conexao
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                // Show(Ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
